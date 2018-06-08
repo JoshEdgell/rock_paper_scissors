d3db13f17ee0eef9eb7f5338ed1f5117381d532e
JoshEdgell <josh.0.edgell@gmail.com>
2018-06-08 13:53:15 -0400

    animated fists

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(()=>{
-  
+
+  const $startButton = $('#startButton');
+  const $playerFist = $('#playerFist');
+  const $computerFist = $('#computerFist');
+
+  const animateFists = function(){
+    $playerFist.attr('id','startPlayer');
+    $computerFist.attr('id','startComputer')
+  }
+
+  const removePlayerFist = function() {
+    console.log('remove function invoked');
+    $playerFist.remove();
+  }
+
+  $startButton.on('click', function(){
+    animateFists();
+    setTimeout(function(){
+      $playerFist.remove();
+      $computerFist.remove();
+    }, 2000);
+  })
+
 })
